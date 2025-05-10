6553228c46e61cc3fb2f0f44993208cb330f741e
Nathan Rossi <nathanrossi30@gmail.com>
2025-05-09 21:07:32 -0300

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,67 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <windows.h>
 
+using namespace std;
+
+bool Tecla_pressionada(){
+    return _kbhit();
+}
+
+char Tecla(){
+    return _getch();
+}
+
+void print_tela(int altura, int largura){ //a tela será de 20x20, mas deixar isso varialvel depois
+    system("cls");
+    string tela;
+    for (int i=-1;i<=20;i++){
+        for (int j=-1;j<=20;j++){
+            if (i == -1 || i == 20 || j == -1 || j == 20){
+                tela += "#";
+            }
+            else if (i == altura && j == largura){
+                tela += "O";
+            }
+            else {
+                tela += " ";
+            }
+        }
+        tela += "\n";
+    }
+    cout << tela;
+}
+    
+int main() {
+    system("cls");
+    int altura = 10;
+    int largura = 10;
+
+    while (1){
+        print_tela(altura, largura);
+        if (Tecla_pressionada()){
+            char c = Tecla();
+
+                switch (c){
+                    case 'a':
+                      largura--;
+                      break;
+                    case 'd':
+                        largura++;
+                       break;
+                    case 'w':
+                     altura--;
+                        break;
+                    case 's':
+                      altura++;
+                     break;
+                    
+                }
+             
+        }
+        Sleep(200);
+    }
+    return 0;
+
+
+}
