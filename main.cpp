74bc8bdc1a1ec6b3e72c45fd44e95162a8f648ba
Nathan Rossi <nathanrossi30@gmail.com>
2025-05-09 23:03:14 -0300

    update_movement

--- main.cpp
+++ main.cpp
@@ -9,7 +9,20 @@ bool Tecla_pressionada(){
 }
 
 char Tecla(){
-    return _getch();
+    if (GetAsyncKeyState(0x57) & 0x8000) {
+        return 'w';
+    }
+    else if (GetAsyncKeyState(0x41) & 0x8000) {
+        return 'a';
+    }
+    else if (GetAsyncKeyState(0x53) & 0x8000) {
+        return 's';
+    }
+    else if (GetAsyncKeyState(0x44) & 0x8000) {
+        return 'd';
+    }
+    else {return ' ';}
+    //return _getch();
 }
 
 void print_tela(int altura, int largura){ //a tela será de 20x20, mas deixar isso varialvel depois
@@ -18,13 +31,13 @@ void print_tela(int altura, int largura){ //a tela será de 20x20, mas deixar is
     for (int i=-1;i<=20;i++){
         for (int j=-1;j<=20;j++){
             if (i == -1 || i == 20 || j == -1 || j == 20){
-                tela += "#";
+                tela += "# ";
             }
             else if (i == altura && j == largura){
-                tela += "O";
+                tela += "O ";
             }
             else {
-                tela += " ";
+                tela += "  ";
             }
         }
         tela += "\n";
@@ -36,11 +49,32 @@ int main() {
     system("cls");
     int altura = 10;
     int largura = 10;
+    char c = 'a';
+    srand(time(0));
+    bool apple = true;
+    int apple_x = rand() % 20;
+    int apple_y = rand() % 20;
+
+    cout << "Pressione Qualquer tecla para comecar" << endl;
+    int qualquer;
+    cin >> qualquer;
 
     while (1){
         print_tela(altura, largura);
-        if (Tecla_pressionada()){
-            char c = Tecla();
+        altura = altura % 20;
+        if (altura < 0){
+            altura = 20 + altura;
+        }
+        largura = largura % 20;
+        if (largura < 0){
+            largura = 20 + largura;
+        }
+        char tecla_anterior = c;
+        c = Tecla();
+        if (c==' '){
+            c = tecla_anterior;
+        }
+
 
                 switch (c){
                     case 'a':
@@ -55,10 +89,11 @@ int main() {
                     case 's':
                       altura++;
                      break;
+                    default:
+                        break;
                     
                 }
              
-        }
         Sleep(200);
     }
     return 0;
